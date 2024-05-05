a72ef42411f0d6a53dead7b0d36dec24c4a43f7b
loco <loco66666@gmail.com>
2024-05-05 16:11:11 +0800

    test

--- src/main/java/com/gtnewhorizons/angelica/config/AngelicaConfig.java
+++ src/main/java/com/gtnewhorizons/angelica/config/AngelicaConfig.java
@@ -29,19 +29,16 @@ public class AngelicaConfig {
 
     @Config.Comment("Enable Angelica's test blocks")
     @Config.DefaultBoolean(false)
-    @Config.Ignore()
     public static boolean enableTestBlocks;
 
     @Config.Comment("Enable Iris Shaders [Requires Sodium]")
     @Config.DefaultBoolean(false)
     @Config.RequiresMcRestart
-    @Config.Ignore()
     public static boolean enableIris;
 
     @Config.Comment("Enable MCPatcherForge features, still in Alpha. Individual features are toggled in mcpatcher.json")
     @Config.DefaultBoolean(false)
     @Config.RequiresMcRestart
-    @Config.Ignore()
     public static boolean enableMCPatcherForgeFeatures;
 
     @Config.Comment("Replace some vanilla render paths with more optimized versions. Disable if you encounter mixin conflicts.")
